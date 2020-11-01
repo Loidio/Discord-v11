eedd8a06cdb7c6f21c185183fe206f9439c83792
Loidio <73287770+Loidio@users.noreply.github.com>
2020-11-01 16:14:46 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,3 @@
-
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
@@ -6,25 +5,11 @@ const config = require('../config.json');
 
 var prefix = config.prefix;
 
+
 module.exports = client => {
-var oyun = [
-        "!yardım | Komudu ile bütün komutları görebilirsiniz. | Sonoyuncu Developers 🔥",
-        "!destek | Destek Sunucumuza Gelebilirsiniz | Sonoyuncu Developers 🔥",
-        "YouTube Yakup Sonoyuncu | Sonoyuncu Developers 🔥",
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "" );
-        }, 2 * 2000 );
-  
-  
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setActivity(`${client.guilds.size} Sunucu + ${client.users.size} Kullanıcıya Hizmet Veriyor`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-};  
+ 
+  client.user.setActivity(`TEXT`, {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/USER"  
+  });
+};
